09641089aefa04d7e77d085b86e65af312cb9c63
Rebe-nya <filip.slabinaf@gmail.com>
2025-06-06 23:32:00 +0200

    Converting numbers => words

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-s
+const input = document.getElementById("textareaInput").value;
+
+function convertDigitsToWords(input) {
+    const digitWords = {
+        0: "zero",
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine"
+    };
+    let text = input.replace(".", "").split(" ");
+    //console.log(text);
+    for (let i = 0; i < text.length; i++) {
+        if (text[i].length === 1 && text[i] in digitWords) {
+            if (i === 0) {
+                text[i] = digitWords[text[i]].charAt(0).toUpperCase() + digitWords[text[i]].slice(1);
+            }
+            else {
+                text[i] = digitWords[text[i]];
+            }
+        }
+    }
+    return text.join(" ").concat(".");
+}
+console.log("Default: " + input);
+console.log("Converted: " + convertDigitsToWords(input));
